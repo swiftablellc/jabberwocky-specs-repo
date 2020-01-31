288b9c1b08ad1846f5235acb97e12ef1289dc100
broag <jon@swiftable.org>
2020-01-31 13:23:19 -0600

    Releasing Version 0.2.0 of JabberwockyHTKit

---
+++ JabberwockyHTKit/0.2.0/HTKit/Products/Library/Frameworks/HTKit.framework/Headers/HTKit.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTKit.
+FOUNDATION_EXPORT double HTKitVersionNumber;
+
+//! Project version string for HTKit.
+FOUNDATION_EXPORT const unsigned char HTKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTKit/PublicHeader.h>
+
+
